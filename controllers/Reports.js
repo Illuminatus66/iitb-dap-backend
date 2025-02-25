35a222a785d4d7b725bb01b8bf938823d3d188dd
Illuminatus66 <varunsodhani123@gmail.com>
2025-02-25 23:44:33 +0530

    still some changes

--- controllers/Reports.js
+++ controllers/Reports.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import dayjs from "dayjs";
 import Report from "../models/Reports.js";
-import timezone from "dayjs/plugin/timezone";
-import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone.js"
+import utc from "dayjs/plugin/utc.js";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
